467904b6f870ac07dd7890d29a6568efe549ef06
Lavisha Saini <94788919+Itslavisha@users.noreply.github.com>
2025-08-24 13:49:15 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@
 
  function App() {
    return (
+   )
+ }
 -    <div className="min-h-screen bg-neutral-100 text-neutral-900 dark:bg-neutral-900 dark:text-neutral-50">
 -      <div className="p-6 space-y-6">
 -        <h1 className="text-2xl font-bold">Lauv CDN Dashboard</h1>
